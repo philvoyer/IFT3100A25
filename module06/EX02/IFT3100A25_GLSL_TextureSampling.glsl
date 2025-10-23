ce4e8d2c59454b76c30e347475761a7f7781f084
philvoyer <philvoyer@subsequent.dev>
2025-10-23 11:15:49 -0400

    publication exemple 6.2 (GLSL_TextureSampling)

--- module06/EX02/IFT3100A25_GLSL_TextureSampling.glsl
+++ module06/EX02/IFT3100A25_GLSL_TextureSampling.glsl
@@ -1,4 +1,4 @@
-// IFT3100H25_GLSL_TextureSampling.glsl
+// IFT3100A25_GLSL_TextureSampling.glsl
 // Exemple d'une section de code pour échantillonner une texture en GLSL.
 
 // exemple de déclaration des coordonnées de mapping en provenance d'une structure de sommets
